223a130ce2ee6fd4ae0c129448d18e67feed1a36
Leandro <atiladevelop@gmail.com>
2024-01-30 01:43:29 -0300

    chore: working on pokemon card

---
+++ src/app/dashboard/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,16 @@
+import { SimplePokemon } from '@/app/pokemons';
+import { PokemonCard } from './PokemonCard';
+
+interface Props {
+  pokemons: SimplePokemon[];
+}
+
+export const PokemonGrid = ({ pokemons }: Props) => {
+  return (
+    <div className='flex flex-wrap gap-10 items-center justify-center'>
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+      ))}
+    </div>
+  );
+};
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,32 @@
-export default function PokemonsPage() {
+import { SimplePokemon } from '@/app/pokemons';
+import { PokemonsResponse } from '@/app/pokemons/interfaces/pokemons-response';
+import { PokemonGrid } from './components/PokemonGrid';
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
   return (
-    <div>
-      <h1>Hello PokemonsPage</h1>
+    <div className='flex flex-col'>
+      <span className='text-5xl my-2'>
+        Lista de Pokemons <small>estático</small>
+      </span>
+      <PokemonGrid pokemons={pokemons} />
     </div>
   );
 }
